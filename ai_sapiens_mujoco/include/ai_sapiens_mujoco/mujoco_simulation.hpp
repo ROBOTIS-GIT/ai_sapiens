b2f2386115f4e65cd0c19c1fbd209f7bbfe9808a
Kiwoong Park <abrahamola80@gmail.com>
2026-08-03 15:50:58 +0900

    Keep gantry upright when reattaching
    Signed-off-by: Kiwoong Park <abrahamola80@gmail.com>

--- ai_sapiens_mujoco/include/ai_sapiens_mujoco/mujoco_simulation.hpp
+++ ai_sapiens_mujoco/include/ai_sapiens_mujoco/mujoco_simulation.hpp
@@ -101,7 +101,7 @@ private:
   bool gantry_released_{false};
   double gantry_target_z_{0.0}, gantry_speed_{0.0};
   std::array<mjtNum, 3> gantry_to_robot_pos_{};
-  std::array<mjtNum, 4> gantry_to_robot_quat_{1.0, 0.0, 0.0, 0.0};
+  std::array<mjtNum, 4> gantry_upright_quat_{1.0, 0.0, 0.0, 0.0};
 };
 
 }  // namespace ai_sapiens_mujoco
