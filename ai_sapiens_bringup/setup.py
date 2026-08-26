ebcc2f935f8bdaee9e454221870eb001e5d4a1cc
Kiwoong Park <abrahamola80@gmail.com>
2026-08-26 18:11:11 +0900

    Align bringup Python package version
    Signed-off-by: Kiwoong Park <abrahamola80@gmail.com>

--- ai_sapiens_bringup/setup.py
+++ ai_sapiens_bringup/setup.py
@@ -33,7 +33,7 @@ def package_files(directory):
 
 setup(
     name=package_name,
-    version='0.1.2',
+    version='0.2.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
